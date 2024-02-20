b40e8cb56dd21af508c5769e45fbb86d8da14ca0
danielnascimentotomaz <danielsabio1000@hotmail.com>
2024-02-19 21:43:27 -0300

    add Anotando as Classes de Modelo com as Anotações JPA (jakarta.persistence)

--- credit-application-system/src/main/kotlin/me/dio/creditapplicationsystem/entity/Credit.kt
+++ credit-application-system/src/main/kotlin/me/dio/creditapplicationsystem/entity/Credit.kt
@@ -1,17 +1,35 @@
 package me.dio.creditapplicationsystem.entity
 
+import jakarta.persistence.*
 import me.dio.creditapplicationsystem.enumerate.Status
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.*
 
+@Entity
+@Table(name = "tb_credit")
 data class Credit(
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id : Long? = null,
+
+    @Column(nullable= false,unique= true)
     val creditCode: UUID= UUID.randomUUID(),
+
+    @Column(nullable= false)
     val creditValue: BigDecimal=BigDecimal.ZERO,
+
+    @Column(nullable= false)
     val dayFirstInstallment: LocalDate,
+
+    @Column(nullable= false)
     val numberOfInstallment:Int = 0,
+
+    @Enumerated
     val status: Status = Status.IN_PROGRESS,
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id") //Caso eu não especifique o nome da coluna, ele vai pegar o nome da propriedade
     val customer: Customer? = null
 
 
--- credit-application-system/src/main/kotlin/me/dio/creditapplicationsystem/entity/Customer.kt
+++ credit-application-system/src/main/kotlin/me/dio/creditapplicationsystem/entity/Customer.kt
@@ -1,19 +1,38 @@
 package me.dio.creditapplicationsystem.entity
 
+import jakarta.persistence.*
 import java.math.BigDecimal
 
+@Entity
+@Table(name = "tb_customer")
 data class Customer(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null,
+
+    @Column(nullable= false)
     var firstName: String ="",
+
+    @Column(nullable= false)
     var lastName: String = "",
+
+    @Column(nullable= false,unique=true)
     val cpf: String,
+
+    @Column(nullable= false,unique=true)
     var email: String,
+
+    @Column(nullable= false)
     var password: String = "",
+
+    @Column(nullable= false)
     var income: BigDecimal,
 
+    @Column(nullable= false)
+    @Embedded
     var address:Address = Address(),
 
-
+    @Column(nullable= false)
+    @OneToMany(fetch = FetchType.LAZY, cascade = [CascadeType.REMOVE],mappedBy= "customer")
     var credits: List<Credit> = mutableListOf()
 
 )
